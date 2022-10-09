bcf1225bdc7287ee0841586c00ba15326e5a5993
gmaaddi <grm76@drexel.edu>
2022-10-09 14:00:15 -0400

    :tada: Create initial php files - assignment 1
    index.php; header.php; footer.php

---
+++ final/index.php
@@ -0,0 +1,4 @@
+<?php include_once '../final/components/header.php'; ?>
+<?php $variable = 'Assignment - 01'; ?>
+<h1><?php echo $variable; ?></h1>
+<?php include_once '../final/components/footer.php'; ?>
