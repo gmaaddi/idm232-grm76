b3f3a8f82e6e87962db470cb790555962248279d
gmaaddi <grm76@drexel.edu>
2022-11-01 13:53:27 -0400

    :bento: Design header & footer

--- final/components/footer.php
+++ final/components/footer.php
@@ -1,2 +1,13 @@
+<div class="bottom">
+    <div class="bottom-logo logo">
+        <h3><span class="pink">Delish</span></h3>
+        <h3><span class="orange">Dishes</span></h3>
+    </div>
+
+    <p> &copy; Gianna Maaddi <?php echo date('Y'); ?></p>
+</div>
+
 </body>
+
+<script src="../final/dist/scripts/main.js"></script>
 </html>
