09f64a54e9416ee0e958e7c40cf880272270a39c
gmaaddi <grm76@drexel.edu>
2022-11-12 10:32:43 -0500

    :bento: Hamburger menu JS

--- final/dist/scripts/main.js
+++ final/dist/scripts/main.js
@@ -1,5 +1,8 @@
+console.log("Script Loaded");
+
 const bar = document.getElementById("bar");
 const menu = document.getElementById("menu");
 bar.addEventListener("click", () => {
   menu.classList.toggle("show-menu")
+  console.log('clicked hamburger menu');
 })
