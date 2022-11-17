00d2c6b93d4576c51cc823c0b327e9054f6ac3f7
gmaaddi <grm76@drexel.edu>
2022-11-17 16:07:55 -0500

    :rocket: Add delete page

---
+++ final/admin/recipes/delete.php
@@ -0,0 +1,18 @@
+<?php
+include_once __DIR__ . '/../../app.php';
+if (isset($_GET['Id'])) {
+    $Id = $_GET['Id'];
+} else {
+    die('ID is needed in URL');
+}
+
+$query = "DELETE FROM recipes WHERE Id = {$Id}";
+$result = mysqli_query($db_connection, $query);
+
+// Check there are no errors with our SQL statement
+if ($result) {
+    redirect_to('../../final/list.php');
+} else {
+    $error_message = 'Could Not Delete Recipe: ' . mysqli_error($db_connection);
+    redirect_to('/admin/recipes?error=' . $error_message);
+}
