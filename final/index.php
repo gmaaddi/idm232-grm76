f9fa05b887cf12f28e6168958275102c751ed0b1
gmaaddi <grm76@drexel.edu>
2022-11-20 16:15:00 -0500

    :bento: Add banner content

--- final/index.php
+++ final/index.php
@@ -4,7 +4,23 @@ include_once __DIR__ . '/app.php';
 $page_title = 'Home';
 include_once __DIR__ . '/_components/header.php';
 ?>
+
+<div class="top-container">
+    <div class="top-container-wrapper">
+        <div class="top-container__index">
+            <h1 class="index-title"><span class="pink">Delish</span><span class="orange">Dishes</span> <br>for every meal</h1>
+            <h3 class="index-description">Browse or search to find recipes perfect for each meal. What will you make next?</h3>
+        </div>
+
+        <div class="top-container-img">
+            <img src="dist/images/index-dishes.png"></img>
+         </div>
+    </div>
+</div>
+
+
 <h3 class="assignment-title">Alpha Assignment</h3>
 <h3 class="assignment-link"><li><a href="recipe_details.php">Click here to access Recipe Detail Page</a></li></h3>
+
 <?php include_once __DIR__ . '/_components/footer.php';
 ?>
