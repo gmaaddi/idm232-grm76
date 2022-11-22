c60f60fa50a18da29b9d1d57d2c23497557da1c8
gmaaddi <grm76@drexel.edu>
2022-11-22 12:16:22 -0500

    :bento: Link to recipes page

--- final/_components/header.php
+++ final/_components/header.php
@@ -34,7 +34,7 @@ $document_title = $page_title . ' | ' . $site_title; // Home | DelishDishes
   </div>
     
   <ul class="menu" id="menu">
-    <h5 class="menu-item"><li><a href="#">Recipes</a></li></h5>
+    <h5 class="menu-item"><li> <a href="<?php echo site_url(); ?>/recipes.php">Recipes</a></li></h5>
     <h5 class="menu-item"><li><a href="<?php echo site_url(); ?>/search.php">Search</a></li></h5>
     <h5 class="menu-item"><li><a href="<?php echo site_url(); ?>/list.php">Account</a></li><h5>
   </ul>
