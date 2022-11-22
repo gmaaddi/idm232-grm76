22c80ec70badb63464df4b33cefe61807ecc4d9d
gmaaddi <grm76@drexel.edu>
2022-11-22 12:15:52 -0500

    :bento: Remove static recipe details link

--- final/index.php
+++ final/index.php
@@ -28,9 +28,5 @@ include_once __DIR__ . '/_components/header.php';
     </div>
 </div>
 
-
-<h3 class="assignment-title">Alpha Assignment</h3>
-<h3 class="assignment-link"><li><a href="recipe_details.php">Click here to access Recipe Detail Page</a></li></h3>
-
 <?php include_once __DIR__ . '/_components/footer.php';
 ?>
