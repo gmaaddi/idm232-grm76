1aa170b7631f97e473327aadf7ebc79b73240454
gmaaddi <grm76@drexel.edu>
2022-11-27 08:23:07 -0500

    :bento: Display featured recipes

--- final/index.php
+++ final/index.php
@@ -21,7 +21,46 @@ include_once __DIR__ . '/_components/header.php';
 <div class="main">
     <div class="featured-container">
         <h3 class="featured-text">Featured Recipes</h3>
-        <?php include __DIR__ . '/_components/card.php'; ?>
+        <?php
+$recipe_results = get_recipes();
+if (!isset($recipe_results)) {
+    echo '$recipe results variable is not defined. Please check the code.';
+}
+?>
+
+<div class="card-container">
+<?php
+    $site_url = site_url();
+    $query = "SELECT * FROM recipes WHERE Id = 5 OR Id = 17 OR Id = 28 OR Id = 35";
+    $result = mysqli_query($db_connection, $query);
+    $check_recipe = mysqli_num_rows($result) > 0;
+
+    if($check_recipe)
+    {
+        while($recipe_row = mysqli_fetch_array($result))
+        {
+            echo
+            "
+                    <div class='card'>
+                        <a class='card-link' href='{$site_url}/recipe-details.php?Id={$recipe_row['Id']}'>
+                        <img src='{$site_url}{$recipe_row['Image_path']}' class='card-img' alt='recipe-image'> 
+                        <h5 class='card-title'>{$recipe_row['Recipe_name']}</h5>
+                        </a>
+                    </div>
+            ";
+        }
+    }
+    else
+    {
+        echo "No recipe found";
+    }
+    // if ($result->num_rows > 0) {
+    //     $recipe_results = true;
+    // } else {
+    //     $recipe_results = false;
+    // }
+?>
+</div>
         <div class="all-btn">
             <a href="<?php echo site_url(); ?>/recipes.php"><button><h5>View All Recipes</h5></button></a>
         </div>
