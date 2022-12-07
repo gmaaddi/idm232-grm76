5ac60c7e1a4d5498af63500f55b5b56e7d5242a0
gmaaddi <grm76@drexel.edu>
2022-12-07 15:21:56 -0500

    :bug: Fix WYSIWYG errors
    see professor email for specifics; updated plugins, toolbar but NOT content style

--- final/_components/footer.php
+++ final/_components/footer.php
@@ -20,8 +20,12 @@
   <script>
     tinymce.init({
       selector: 'textarea',
-      plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect',
-      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+      // plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect',
+      plugins: 'anchor autolink  codesample  link lists searchreplace table visualblocks wordcount',
+      // toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link table | align lineheight | numlist bullist indent outdent |   removeformat',
+      // content-style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+      
       tinycomments_mode: 'embedded',
       tinycomments_author: 'Author name',
       mergetags_list: [
